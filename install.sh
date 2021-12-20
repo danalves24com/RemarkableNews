40094e9ec8c3f8e3a5bf2f1a812e7310c8c79b5d
danalves24com <danalves24@outlook.com>
2021-12-20 19:51:55 +0100

    installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 mkdir $HOME/News
 mkdir $HOME/News/stories
-
+sudo mkdir /usr/bin/.newsmark
+sudo curl --request GET -sL \
+     --url 'https://github.com/danalves24com/RemarkableNews/releases/download/Beta/news-fetching.jar'\
+     --output '/usr/bin/.newsmark/news-fetching.jar'
+sudo touch /usr/bin/newsmark
+sudo echo "java -jar /usr/bin/.newsmark/news-fetching.jar" >> /usr/bin/newsmark
+sudo chmod +x /usr/bin/newsmark
